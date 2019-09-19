e425eefe89bdf2323dce9c6f08903c6451ff01d3
TobiasBms <tobi8678@edu.ucl.dk>
2019-09-19 16:18:51 +0200

    Clean sleve, build recipe and recipelist components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, {useState} from 'react';
-import CounterHooks from "./components/CounterHooks";
+import React from 'react';
 import logo from './logo.svg';
+import RecipeList from "./components/RecipeList";
 import './App.css';
 
 export const ThemeContext = React.createContext();
 
 
 function App() {
-	const [theme, setTheme ] = useState('green');
-  return (  
-  	<ThemeContext.Provider value={{backgroundColor: theme}}>
-  		<CounterHooks initialCount={0} />
-  		<button onClick={() => setTheme(prevTheme => {
-  			return prevTheme == "red" ? 'blue' : "red"
-  		})}>Toggle theme</button>
-  	</ThemeContext.Provider>
-    );
+  return <RecipeList recipes={sampleRecipies} />;
 }
+
+const sampleRecipies = [
+	{
+		id: 1,
+		name: "Plain chicken",
+		servings: 3,
+		cookTime: "1:45",
+		instructions: "1. salt\n 2. oven\n 3. eat"
+	},
+	{
+		id: 2,
+		name: "tacos",
+		servings: 10,
+		cookTime: "4:00",
+		instructions: "1. salt\n 2. oven\n 3. eat"
+	},
+]
 
 export default App;
